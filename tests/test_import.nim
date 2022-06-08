1a58c048bc2ba1d516f1ca5b5f07fb33e3c3665f
Jacek Sieka <jacek@status.im>
2022-06-08 08:10:31 +0200

    work around constness issue

--- tests/test_import.nim
+++ tests/test_import.nim
@@ -5,4 +5,7 @@ import ../bearssl
 # TODO doesn't work from C++ due to missing `export "C"`
 # discard getConfig()
 
-discard ecGetDefault()
+# TODO doesn't work from C++ due to `const`:ness issues
+# discard ecGetDefault()
+
+discard ghashPwr8Get()
