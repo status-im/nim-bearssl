a2dd725eec681cb8c4cd7601c24e994e0c586b53
Jacek Sieka <jacek@status.im>
2022-06-14 13:07:26 +0200

    pem: convenience API

--- bearssl/pem.nim
+++ bearssl/pem.nim
@@ -1,4 +1,40 @@
 import
+  typetraits,
   ./abi/bearssl_pem
 
 export bearssl_pem
+
+func init*(v: var PemDecoderContext) =
+  # Careful, PemDecoderContext items are not copyable!
+  # TODO prevent copying
+  pemDecoderInit(v)
+
+func push*[S](ctx: var PemDecoderContext, data: openArray[S]): int =
+  static: doAssert supportsCopyMem(S)
+  if data.len > 0:
+    let consumed = pemDecoderPush(
+      ctx, unsafeAddr data[0], uint data.len * sizeof(S))
+    int(consumed) div sizeof(S)
+  else:
+    0
+
+func setdest*(
+    ctx: var PemDecoderContext;
+    dest: proc (destCtx: pointer;
+      src: pointer; len: uint) {.cdecl, gcsafe, noSideEffect, raises: [].};
+    destCtx: pointer) =
+  pemDecoderSetdest(ctx, dest, destCtx)
+
+func lastEvent*(ctx: var PemDecoderContext): cint =
+  pemDecoderEvent(ctx)
+
+func name*(ctx: PemDecoderContext): cstring =
+  unsafeAddr ctx.name
+
+func pemEncode*(
+    data: openArray[byte], banner: cstring, flags: cuint = 0): seq[byte] =
+  let bytes = pemEncode(nil, nil, uint data.len, banner, flags)
+  result.setLen(int bytes + 1)
+  discard pemEncode(
+    addr result[0], unsafeAddr data[0], uint data.len, banner, flags)
+  result.setLen(int bytes)
